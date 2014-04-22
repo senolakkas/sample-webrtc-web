8eaf03728d57a6f3235ee1ae65f5f1a9b1d500ef
AndreyPovelichenko <andrey.povelichenko@quickblox.com>
2014-04-22 14:59:46 +0300

    update webrtc plugin

--- main.js
+++ main.js
@@ -176,6 +176,67 @@ function getMediaError(qbID) {
 	videoChat.reject(qbID, chatUser.login);
 }
 
+function onCall(qbID, sessionDescription, sessionID, name, avatar) {
+	/*var win, selector, winName = 'remoteCall' + qbID;
+	
+	if (popups[winName]) {
+		popups[winName].close();
+		delete popups[winName];
+	}
+	
+	popups[winName] = openPopup(winName, {width: 250, height: 280});
+	win = popups[winName];
+	
+	win.onload = function() {
+		selector = $(win.document);
+		selector.find('#acceptCall').click(acceptCall);
+		selector.find('#rejectCall').click(function() {rejectCall(qbID, sessionID)});
+		
+		htmlRemoteCallBuilder(selector, qbID, sessionDescription, sessionID, avatar, name);
+		audio.ring.play();
+		
+		win.onbeforeunload = function() {
+			if (switches.isPopupClosed)
+				rejectCall(qbID, sessionID);
+			switches.isPopupClosed = true;
+		};
+	};*/
+}
+
+function onAccept(qbID) {
+	/*var win = popups['videochat'];
+	getRemoteStream($(win.document));*/
+}
+
+function onReject(qbID) {
+	/*var win = popups['videochat'];
+	if (win)
+		$(win.document).find('#stopCall').hide().parent().find('#doCall').show();*/
+}
+
+function onStop(qbID) {
+	/*var win;
+	
+	win = popups['videochat'];
+	if (win && qbID == $(win.document).find('#stopCall').data('qb')) {
+		switches.isPopupClosed = false;
+		win.close();
+		delete popups['videochat'];
+		
+		videoChat.hangup();
+		videoChat = null;
+	}
+	
+	win = popups['remoteCall' + qbID];
+	if (win) {
+		switches.isPopupClosed = false;
+		win.close();
+		delete popups['remoteCall' + qbID];
+		
+		stopRing(popups);
+	}*/
+}
+
 /* Helpers
 ----------------------------------------------------------*/
 function chooseOpponent(currentLogin) {
