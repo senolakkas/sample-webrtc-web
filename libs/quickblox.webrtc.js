4a0581efc93bfa54588ab56acc4c26623c56829e
AndreyPovelichenko <andrey.povelichenko@quickblox.com>
2014-12-04 01:02:58 +0200

    test commit

--- libs/quickblox.webrtc.js
+++ libs/quickblox.webrtc.js
@@ -8,54 +8,34 @@
 
 var config = {
 	iceServers: [
-	/*	{urls: "stun:stun.l.google.com:19302"},
-		{urls: "turn:turnserver.quickblox.com:3478?transport=udp", username: "", credential: ""},
-		{urls: "turn:turnserver.quickblox.com:3478?transport=tcp", username: "", credential: ""}*/
-		
-		{url:'stun:stun01.sipphone.com'},
-		{url:'stun:stun.ekiga.net'},
-		{url:'stun:stun.fwdnet.net'},
-		{url:'stun:stun.ideasip.com'},
-		{url:'stun:stun.iptel.org'},
-		{url:'stun:stun.rixtelecom.se'},
-		{url:'stun:stun.schlund.de'},
-		{url:'stun:stun.l.google.com:19302'},
-		{url:'stun:23.21.150.121'},
-		{url:'stun:stun1.l.google.com:19302'},
-		{url:'stun:stun2.l.google.com:19302'},
-		{url:'stun:stun3.l.google.com:19302'},
-		{url:'stun:stun4.l.google.com:19302'},
-		{url:'stun:stunserver.org'},
-		{url:'stun:stun.softjoys.com'},
-		{url:'stun:stun.voiparound.com'},
-		{url:'stun:stun.voipbuster.com'},
-		{url:'stun:stun.voipstunt.com'},
-		{url:'stun:stun.voxgratia.org'},
-		{url:'stun:stun.xten.com'},
 		{
-		    url: 'turn:turnserver.quickblox.com:3478?transport=udp',
-		    credential: ' ',
-		    username: ' '
+			"url": "stun:stun.l.google.com:19302"
 		},
 		{
-		    url: 'turn:turnserver.quickblox.com:3478?transport=tcp',
-		    credential: ' ',
-		    username: ' '
+			"url": "stun:stun.anyfirewall.com:3478"
 		},
 		{
-		    url: 'turn:numb.viagenie.ca',
-		    credential: 'muazkh',
-		    username: 'webrtc@live.com'
+			"url": "turn:turn.bistri.com:80",
+			"credential": "homeo",
+			"username": "homeo"
 		},
 		{
-		    url: 'turn:192.158.29.39:3478?transport=udp',
-		    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-		    username: '28224511:1379330808'
+			"url": "turn:turn.anyfirewall.com:443?transport=tcp",
+			"credential": "webrtc",
+			"username": "webrtc"
 		},
 		{
-		    url: 'turn:192.158.29.39:3478?transport=tcp',
-		    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-		    username: '28224511:1379330808'
+			"url": "stun:turn2.xirsys.com"
+		},
+		{
+			"username": "36b7fdaf-524e-4c38-a6d3-b174166fd573",
+			"url": "turn:turn2.xirsys.com:443?transport=udp",
+			"credential": "0371abb5-fa95-4bbe-b282-25e5888513f7"
+		},
+		{
+			"username": "36b7fdaf-524e-4c38-a6d3-b174166fd573",
+			"url": "turn:turn2.xirsys.com:443?transport=tcp",
+			"credential": "0371abb5-fa95-4bbe-b282-25e5888513f7"
 		}
 	]
 };
